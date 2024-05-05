e222a81cdc5a9c584527cffbd8b4bf69fa1a23fe
Sami <saminazirqureshi1@gmail.com>
2024-05-06 00:59:43 +0500

    main

--- frontend/src/Components/login/Login.jsx
+++ frontend/src/Components/login/Login.jsx
@@ -1,6 +1,21 @@
+import { useRef, useContext } from "react";
 import "./login.css";
-
+import {loginCall} from "../../apiCalls.js";
+import {AuthContext} from "../../context/AuthContext.jsx"
 function Login() {
+  const { user, isFetching, dispatch } = useContext(AuthContext);
+  const email = useRef();
+  const password = useRef();
+  const handleClick = (e) => {
+    e.preventDefault();
+    
+    loginCall(
+      { email: email.current.value, password: password.current.value },
+      dispatch
+    );
+    console.log(user, password.current.value, email.current.value)
+  };
+
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -11,15 +26,28 @@ function Login() {
           </span>
         </div>
         <div className="loginRight">
-          <div className="loginBox">
-            <input placeholder="Email" className="loginInput" />
-            <input placeholder="Password" className="loginInput" />
+          <form className="loginBox" onSubmit={handleClick}>
+            <input
+              placeholder="Email"
+               ref={email}
+              required
+              type="email"
+              className="loginInput"
+            />
+            <input
+              placeholder="Password"
+              ref={password}
+              minLength="4"
+              required
+              type="password"
+              className="loginInput"
+            />
             <button className="loginButton">Log In</button>
             <span className="loginForgot">Forgot Password?</span>
             <button className="loginRegisterButton">
               Create a New Account
             </button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
